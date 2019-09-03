50ce1cbb7ffa7be68f71e1b33b3f401a654ff3fb
Hesham Alkurdi <alkurdi.hesham@gmail.com>
2019-09-03 14:13:38 +0200

    First

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+}
+  componentDidMount(){
+    let data = {
+      Title:"twssstt",
+      Auther:[{
+        display: "Hesham Alkurdi",
+        link: "Author",
+        _id: "5d6e47da39343201cd0002f9"
+      }],
+      Date:"2019-02-04",
+      Body:"asdffaasdasdsoo"
+    }
+    fetch('https://hesh.devspace.host/api/collections/save/Articels', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          data: data
+      })
+  })
+  .then(res=>res.json())
+  .then(entry => console.log(entry));
+    fetch('https://hesh.devspace.host/api/collections/get/Articels')
+    .then(res => res.json())
+    .then(res => console.log(res));
+  }
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +48,10 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
+
+
 
 export default App;
